71f633fe437a181f6572e7a10ad53415e8fd4e4b
user2812 <gassama761@gmail.com>
2025-08-10 12:41:52 +0200

    Add files via upload

---
+++ trend_analysis/│   ├── trend_nlp.py
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const │   ├── trend_nlp = () => {
+  return (
+    <div>
+      <h1>│   ├── trend_nlp Component</h1>
+    </div>
+  );
+};
+
+export default │   ├── trend_nlp;
