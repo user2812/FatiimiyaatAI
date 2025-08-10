f79519d6fc5ae81976fee81849679ffb77f805d0
user2812 <gassama761@gmail.com>
2025-08-10 12:40:05 +0200

    Add files via upload

---
+++ sales_forecast/│   ├── data_preparation.py
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const │   ├── data_preparation = () => {
+  return (
+    <div>
+      <h1>│   ├── data_preparation Component</h1>
+    </div>
+  );
+};
+
+export default │   ├── data_preparation;
